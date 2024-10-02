ff76c96f1962422873f5d73fea82b049e8a01330
QuasimKhan <quasimkhanpersonal@gmail.com>
2024-10-02 11:25:52 +0530

    updated

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.get("/api", (req, res) => {
+    res.send("API is running!");
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port http://localhost:${PORT}`);
